fd91df1bbe01b5c3f7e3882e89017a93790a545c
Tinshu Sasi <thetinshusasi@gmail.com>
2019-07-03 05:21:33 +0530

    BST started

---
+++ DataStructures/DataStructures/DataStructures/BinaryTree/BinarySearchTree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.DataStructures.BinaryTree
+{
+    public class BinarySearchTree<T> :IEnumerable<T> where T : IComparable<T>
+    {
+        public BinaryTreeNode<T> Root { get; set; }
+        public int Count { get; private set; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Insert( T node)
+        {
+            if (Root == null)
+            {
+                Root = new BinaryTreeNode<T>(node);
+            }
+            else
+            {
+                AddTo(Root, node);
+            }
+
+
+            Count++;
+        }
+
+        void AddTo(BinaryTreeNode<T> CurrNode, T value)
+        {
+            if (CurrNode.Value.CompareTo(value) >= 0)
+            {
+                if (CurrNode.Right == null)
+                {
+                    CurrNode.Right = new BinaryTreeNode<T>(value);
+                }
+                else
+                {
+                    AddTo(CurrNode.Right, value);
+                }
+            }
+            else 
+            {
+                if (CurrNode.Left == null)
+                {
+                    CurrNode.Left = new BinaryTreeNode<T>(value);
+                }
+                else
+                {
+                    AddTo(CurrNode.Left, value);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ DataStructures/DataStructures/DataStructures/BinaryTree/BinaryTreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.DataStructures
+{
+    public class BinaryTreeNode<TNode> : IComparable<TNode> where TNode : IComparable<TNode>
+    {
+        public int CompareTo(TNode other)
+        {
+            return Value.CompareTo(other);
+
+        }
+
+        public TNode Value { get; private set; }
+        public BinaryTreeNode<TNode> Left { get; set; }
+        public BinaryTreeNode<TNode> Right { get; set; }
+        public BinaryTreeNode(TNode node)
+        {
+            Value = node;
+        }
+    }
+}
+
